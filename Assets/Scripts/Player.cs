e8d2762d28f31a9da4f1671b43d4a5ac108762ba
Iambadatcb <gviduks.cepuliuks@gmail.com>
2024-10-27 16:34:19 +0200

    fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PLayer : MonoBehaviour
+public class Player : MonoBehaviour
 {
     private Vector3 playerPos;
     private Camera mainCamera;
